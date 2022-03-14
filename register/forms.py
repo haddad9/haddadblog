175382c3e71e139245a0d5f9570af8811a733d35
haddad <emha.haddad@gmail.com>
2022-03-14 15:51:04 +0700

    fix: frontend bug fux login register

--- register/forms.py
+++ register/forms.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from dataclasses import field
 from multiprocessing import AuthenticationError
 from django import forms
@@ -8,10 +9,33 @@ from django.contrib.auth.models import User
 class RegistrationForms(UserCreationForm):
    
     firstname = forms.CharField()
-    lastname = forms.CharField()
+   
     
     
     class Meta:
         model = User
-        fields = ['firstname', 'lastname' , 'username','password1','password2']
+        fields = ['firstname', 'username','password1','password2']
+        
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForms, self).__init__(*args, **kwargs)
+
+        self.fields['firstname'].label = "Fullname"
+        
+        self.fields["firstname"].widget.attrs.update(
+            {"placeholder": "Fullname", "autofocus": None}
+        )
+
+       
+        self.fields["username"].widget.attrs.update(
+            {"placeholder": "Username"}
+        )
+        self.fields["password1"].widget.attrs.update(
+            {"placeholder": "Password"}
+        )
+        self.fields["password2"].widget.attrs.update(
+            {"placeholder": "Confirmation Password"}
+        )
+
+        for name, field in self.fields.items():
+            field.widget.attrs["required"] = None
         
